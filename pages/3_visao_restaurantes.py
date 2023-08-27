54666b8f094cdb2339d19ae9392165bd84d35744
Ruiz Roman <85726283+ruizroman@users.noreply.github.com>
2023-08-27 18:40:33 -0300

    path changes

--- pages/3_visao_restaurantes.py
+++ pages/3_visao_restaurantes.py
@@ -202,7 +202,7 @@ def filters(df1):
 # ----------- IMPORTANDO DATASET -----------------------------
 #=============================================================
 
-df_raw = pd.read_csv('dados\train.csv')
+df_raw = pd.read_csv('dados/train.csv')
 
 df1 = clean_code(df_raw)
 
